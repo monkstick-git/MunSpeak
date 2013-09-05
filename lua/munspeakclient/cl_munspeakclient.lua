5ac45d1adc40dad4091231340d9b0882410c861d
Kieron <monkstick@gmail.com>
2013-09-05 20:15:01 +0100

    Updated backend and GUI

--- lua/munspeakclient/cl_munspeakclient.lua
+++ lua/munspeakclient/cl_munspeakclient.lua
@@ -1,5 +1,10 @@
 local playerChannel = "";
 local Channels = {}
+local open = true
+local PanelX = 50
+local PanelY = 50
+local MouseX = 0
+local MouseY = 0
 
 function MunSpeakMute(ply)
 local PlayerEnt = ply
@@ -65,10 +70,12 @@ end
 
 timer.Create("MunSpeakLoop",1,0,MunSpeakLoop)
 
- 
 net.Receive("MunSpeakChannels",function()
 Channels = net.ReadTable()
 PrintTable(Channels)
+
+
+
 end)
 
 net.Receive("MunSpeakShowUi",function()
@@ -76,30 +83,74 @@ ShowPanel()
 end)
 
 function ShowPanel()
-
+	
 	MunSpeakPanel = vgui.Create( "DFrame" )
-	MunSpeakPanel:SetPos( 50, 50 )
-	MunSpeakPanel:SetSize( 310, 340 )
+	MunSpeakPanel:SetPos( PanelX, PanelY )
+	MunSpeakPanel:SetSize( 450, 340 )
 	MunSpeakPanel:SetTitle( "MunSpeak - Version 0.1" )
 	MunSpeakPanel:SetVisible( true )
 	MunSpeakPanel:SetDraggable( true )
 	MunSpeakPanel:ShowCloseButton( true )
 	MunSpeakPanel:MakePopup()
-	 
+	function MunSpeakPanel:OnClose()
+	open = !open
+	PanelX, PanelY = MunSpeakPanel:GetBounds()
+	MouseX, MouseY = input.GetCursorPos( )
+	end
+	
+	MunSpeakJoin = vgui.Create("DButton", MunSpeakPanel)
+	MunSpeakJoin:SetPos( 340, 35 )
+	MunSpeakJoin:SetSize( 75, 30 )
+	MunSpeakJoin:SetText("Join")
+	function MunSpeakJoin.DoClick( self )
+	local Pass = ""
+	if(MunSpeakTextInput:GetValue() == "password...") then Pass = "" else Pass = MunSpeakTextInput:GetValue() end
+	if( MunSpeakParent:GetSelectedItem( )) then
+		net.Start("MunSpeakClientJoin")
+		net.WriteTable({LocalPlayer(),MunSpeakParent:GetSelectedItem( ).Channel,Pass})
+	else
+	
+	end
+	net.SendToServer()
+	MunSpeakPanel:Close()
+	timer.Simple(0.001,function() MunSpeakOpenUI() end)
+	end
+	
+	MunSpeakTextInput = vgui.Create( "DTextEntry", MunSpeakPanel )	-- create the form as a child of frame
+	MunSpeakTextInput:SetPos( 340, 75 )
+	MunSpeakTextInput:SetSize( 75, 30 )
+	MunSpeakTextInput:SetText( "password..." )
+	MunSpeakTextInput.OnEnter = function( self )
+	local Pass = ""
+	if(self:GetValue() == "password...") then Pass = "" else Pass = self:GetValue() end
+	if( MunSpeakParent:GetSelectedItem( )) then
+		net.Start("MunSpeakClientJoin")
+		net.WriteTable({LocalPlayer(),MunSpeakParent:GetSelectedItem( ).Channel,Pass})
+	else
+	
+	end
+	net.SendToServer()
+	MunSpeakPanel:Close()
+	timer.Simple(0.001,function() MunSpeakOpenUI() end)
+	--chat.AddText( self:GetValue() )	-- print the form's text as server text
+	end
+	
 	MunSpeakParent = vgui.Create( "DTree", MunSpeakPanel )
 	 
 	MunSpeakParent:SetPos( 5, 30 )
 	MunSpeakParent:SetPadding( 5 )
 	MunSpeakParent:SetSize( 300, 300 )
-
+	MunSpeakParent:SetExpanded(true)
 
 	for k,v in pairs(Channels) do
-		local MunSpeakChild = MunSpeakParent:AddNode( k ,"icon16/folder_user.png")
-	
+		MunSpeakChild = MunSpeakParent:AddNode( k ,"icon16/folder_user.png")
+		MunSpeakChild.Channel = k
+		MunSpeakChild:SetExpanded(true)
 	for key,value in pairs(v.Members) do
 		local TempNick = "nil"
-		if(value == NULL) then MsgN("VALUE NOT FUCKING VALID") else TempNick = value:Nick() end
-		local cnode = MunSpeakChild:AddNode(TempNick,"icon16/user.png")
+		if(value == NULL) then MsgN("Value not Valid!") else TempNick = value:Nick() end
+		cnode = MunSpeakChild:AddNode(TempNick,"icon16/user.png")
+		cnode.Player = value
 --	print(key.."  "..value:Nick())
 	end
 	
@@ -108,10 +159,12 @@ end
 
 hook.Add("Initialize","MunSpeakInit",function() timer.Simple(2,ShowPanel) print("SHOWING THE PANEL") end)
 
-	-- for k,v in pairs(Channels) do
-		-- local MunSpeakChild = MunSpeakParent:AddNode( k )
-			-- for k1,v1 in pairs(v) do
-				-- A=A+1
-				-- local cnode = MunSpeakChild:AddNode(v1[A]:Nick())
-			-- end
-	-- end
+
+concommand.Add("munspeak", function()
+MunSpeakOpenUI()
+end)
+
+function MunSpeakOpenUI()
+	open = !open
+	if(open) then ShowPanel() gui.SetMousePos(MouseX, MouseY) else MouseX, MouseY = input.GetCursorPos( )	PanelX, PanelY = MunSpeakPanel:GetBounds() MunSpeakPanel:Close() end
+end
--- lua/munspeakserver/munspeakmain.lua
+++ lua/munspeakserver/munspeakmain.lua
@@ -1,75 +1,74 @@
-﻿MunSpeakChannels = {}
-MunSpeakChannels["Default"] = {Owner = "Server",Members = {},Password = ""}
+﻿MunSpeak = {}
+MunSpeakChannels = {}
+MunSpeakAdmins = {}
 util.AddNetworkString("MunSpeakChannels")
 util.AddNetworkString("MunSpeakShowUi")
+util.AddNetworkString("MunSpeakClientJoin")
 
-function MunSpeak.CreateChannel(ply,msg) 
+function MunSpeak.ChatMessages(ply,msg) -- Chat Messages
 	local Message = string.Explode(" ",msg)
-
-	if(Message[1]=="/channel") then
-		if(not Message[3]) then ChannelPass = "" else ChannelPass = Message[3] end
-		
-		if(not MunSpeakChannels[Message[2]]) then
-			MunSpeakChannels[Message[2]] = {Owner = ply:SteamID(),Members = {},Password = ChannelPass} PrintMessage(HUD_PRINTTALK,ply:Nick().." Created channel "..Message[2])
-		else 
-			if(ChannelPass != MunSpeakChannels[Message[2]].Password) then ply:PrintMessage(HUD_PRINTTALK,"Password is incorrect!") return false else PrintMessage(HUD_PRINTTALK,ply:Nick().." Joined channel "..Message[2]) end
-			
-		end
-		
-		
-		CheckChannels(ply)
-		table.insert(MunSpeakChannels[Message[2]].Members,ply)
-		ply:SetNWString("Channel",Message[2])
-		
-		MunTalkSendChannels()
-		
+	local Target = 0
+	
+	if(Message[1]=="/createchannel") then
+		MunSpeak.CreateChannel(ply,Message[2],Message[3])
+		return ""
+	end
+	
+	if(Message[1]=="/joinchannel") then
+		MunSpeak.JoinChannel(ply,Message[2],Message[3])
 		return ""
 	end
 	
 	if(Message[1]=="/leave") then
-		if(table.HasValue(MunSpeakChannels["Default"],ply)) then PrintMessage(HUD_PRINTTALK,"PLAYER IS ALREADY IN THAT TABLE") return end
-		CheckChannels(ply)
-		table.insert(MunSpeakChannels["Default"].Members,ply)
-		ply:SetNWString("Channel","Default")
-		MunTalkSendChannels()
+		MunSpeak.Leave(ply)
 		return ""
-	end
-	
-	if(Message[1]=="/listchannels") then
-		for k,v in pairs(MunSpeakChannels) do
-		local TrueFalse = ""
-		if string.len(MunSpeakChannels[k].Password)>0 then TrueFalse = ": *Passworded*" else TrueFalse = "" end
-		local TempString = k.." "..TrueFalse
-			MsgN(TempString)
-			for key,value in pairs(MunSpeakChannels[k].Members) do
-			MsgN(string.rep(" ",10)..value:Nick())
-			end
-		end
 	end
 
 	if(Message[1]=="/munspeak") then
-		MunTalkShowUi(ply)
+		MunSpeak.ShowUI(ply)
+	end
+	
+	if(Message[1]=="/del") then
+	MunSpeak.Delete(ply,Message[2])
+	end
+	
+	if(Message[1]=="/json") then
+		MunSpeak.WriteTable()
+	end
+	
+	if(Message[1]=="/read") then
+		MunSpeak.ReadTable()
+	end
+	
+	if(Message[1]=="/move") then
+		local PlayerData = MunSpeak.FindPlayer(Message[2])
+		MunSpeak.MovePlayer(ply,PlayerData,Message[3])
+	end
+	
+	if(Message[1]=="/createadmin") then
+		 MunSpeak.CreateAdmin(ply,MunSpeak.FindPlayer(Message[2]))
+	end
+	
+	if(Message[1]=="/isadmin") then
+		PrintMessage(HUD_PRINTTALK,tostring(MunSpeak.IsAdmin(MunSpeak.FindPlayer(Message[2]))))
 	end
 end
 
-hook.Add("PlayerSay", "munspeakcreate", MunSpeak.CreateChannel)
-
-function MunSpeak.PlayerHasConnected(ply)
+function MunSpeak.PlayerHasConnected(ply) -- When a player first joins
 	if(!ply.MunTalkSpawned) then
+	
 	timer.Simple(1,function()
 		if(table.HasValue(MunSpeakChannels["Default"].Members,ply)) then PrintMessage(HUD_PRINTTALK,"PLAYER IS ALREADY IN THAT TABLE") return end
 		table.insert(MunSpeakChannels["Default"].Members,ply)
 		ply:SetNWString("Channel","Default")
-		MunTalkSendChannels()
+		MunSpeak.SendChannels()
 		ply.MunTalkSpawned = true
 	end)
 
 	end
 end
-
-hook.Add("PlayerSpawn","MunTalkPlayerHasConnected",MunSpeak.PlayerHasConnected)
  
-function CheckChannels(ply)
+function MunSpeak.CheckChannels(ply) -- Check the channels to make sure you are only in one channel
 		for k,v in pairs(MunSpeakChannels) do
 			for k2,v2 in pairs(v) do
 				if(table.HasValue(MunSpeakChannels[k].Members,ply)) then 
@@ -81,14 +80,203 @@ function CheckChannels(ply)
 		end
 end 
 
-function MunTalkSendChannels()
+function MunSpeak.SendChannels() -- Send the Channels to the client
 		net.Start("MunSpeakChannels")
 		net.WriteTable(MunSpeakChannels)
 		net.Broadcast()
 end
 
-
-function MunTalkShowUi(ply)
+function MunSpeak.ShowUI(ply) -- Tell the Client to open its UI
 		net.Start("MunSpeakShowUi")
 		net.Send(ply)
 end
+
+function MunSpeak.CreateChannel(ply,name,pass) -- Create channel.  PlayerData and Name of channel are REQUIRED.  pass is optional.
+		local ChannelPass = ""
+		if(not pass) then ChannelPass = "" else ChannelPass = pass end
+		
+		if(not MunSpeakChannels[name]) then
+			MunSpeakChannels[name] = {Owner = ply:SteamID(),Members = {},Password = ChannelPass} PrintMessage(HUD_PRINTTALK,ply:Nick().." Created channel "..name)			
+		end
+		
+		MunSpeak.SendChannels()
+		MunSpeak.WriteTable()
+		
+		return ""
+end
+
+function MunSpeak.JoinChannel(ply,name,password,adminrequest) -- Joining a channel
+	local ChannelPass = ""
+	if(not password) then ChannelPass = "" else ChannelPass = password end
+	
+	if(ChannelPass != MunSpeakChannels[name].Password and not adminrequest) then
+		ply:PrintMessage(HUD_PRINTTALK,"Password is incorrect!") return false
+	else
+		PrintMessage(HUD_PRINTTALK,ply:Nick().." Joined channel "..name)
+	end
+
+	MunSpeak.CheckChannels(ply)
+	table.insert(MunSpeakChannels[name].Members,ply)
+	ply:SetNWString("Channel",name)
+			
+	MunSpeak.SendChannels()
+
+end
+
+function MunSpeak.Leave(ply) -- Leave a channel and go back to the Default channel.
+	if(table.HasValue(MunSpeakChannels["Default"],ply)) then 
+		PrintMessage(HUD_PRINTTALK,"PLAYER IS ALREADY IN THAT TABLE") return 
+	end
+	
+	MunSpeak.CheckChannels(ply)
+	table.insert(MunSpeakChannels["Default"].Members,ply)
+	ply:SetNWString("Channel","Default")
+	MunSpeak.SendChannels()
+end
+
+function MunSpeak.Delete(ply,Channel) -- Delete channel. Player must either Own the channel, ply:IsAdmin() or MunSpeak.IsAdmin(ply)
+	for k,v in pairs(MunSpeakChannels) do
+		if tostring(k) == tostring(Channel) then
+			if((MunSpeakChannels[Channel].Owner == ply:SteamID()) or ply:IsAdmin() or MunSpeak.IsAdmin(ply)) then
+				PrintMessage(HUD_PRINTTALK,ply:Nick().." Has deleted Channel: "..Channel) 
+				MunSpeakChannels[Channel]=nil
+				MunSpeak.SendChannels()
+			else
+				ply:PrintMessage(HUD_PRINTTALK,ply:Nick().."You do not have permission to delete that channel")
+			end
+		end 
+	end
+end
+
+function MunSpeak.SmallTable() -- Creates a smaller version of the ChannelTable.  returns it.
+	local Ctable = MunSpeakChannels
+	local TempTable = {}
+		for k,v in pairs(Ctable) do
+			if table.Count(Ctable[k].Members) > 0 then
+				TempTable[k] = v
+			end
+		end
+		
+		for k,v in pairs(TempTable)do
+			MsgAll(k)
+		end
+	return TempTable
+end
+
+function MunSpeak.WriteTable() -- Writes the current MunSpeak channel layouts and passwords to a json string, which is located in data/MunSpeakTable.txt
+	local Bench1 = SysTime()
+	MsgAll("[MunSpeak] - Writing table to JSON...")
+	local Ctable = MunSpeakChannels
+	for k,v in pairs(Ctable) do
+		Ctable[k].Members[v] = nil
+	end
+
+	
+	local MunSpeakTempString = util.TableToJSON(Ctable)
+	file.Write("MunSpeakTable.txt",MunSpeakTempString)
+	MsgAll("[MunSpeak] - Finished Writing JSON table.  Took: "..(SysTime() - Bench1))
+end
+
+function MunSpeak.ReadTable()	-- Reads the JSON table from data/MunSpeakTable.txt else creates the default channel and creates the json file.
+	if(file.Exists("MunSpeakTable.txt", "DATA")) then
+		MunSpeakChannels = util.JSONToTable(file.Read( "MunSpeakTable.txt", "DATA" ))
+	else
+		MunSpeakChannels["Default"] = {Owner = "Server",Members = {},Password = ""}
+		MunSpeak.WriteTable()
+	end
+	
+	return MunSpeakChannels
+	
+end
+
+function MunSpeak.WriteAdmins() -- Writes all MunSpeak admins to a json text file.
+	local Bench1 = SysTime()
+	MsgAll("[MunSpeak] - Writing Admins to JSON...")
+	local Ctable = MunSpeakAdmins
+	
+	local MunSpeakTempString = util.TableToJSON(Ctable)
+	file.Write("MunSpeakAdmins.txt",MunSpeakTempString)
+	MsgAll("[MunSpeak] - Finished Writing JSON table ADMINS.  Took: "..(SysTime() - Bench1))
+end
+
+function MunSpeak.ReadAdmins() -- Reads the MunSpeakAdmins.txt JSON string to get all MunSpeak Admins
+	if(file.Exists("MunSpeakAdmins.txt", "DATA")) then
+		MunSpeakAdmins = util.JSONToTable(file.Read( "MunSpeakAdmins.txt", "DATA" ))
+	else
+		MunSpeakAdmins = {"Server"}
+		MunSpeak.WriteAdmins()
+	end
+end
+
+function MunSpeak.IsAdmin(ply)
+	if(table.HasValue(MunSpeakAdmins,ply:SteamID()) == true) then
+		return true
+	else
+		return false
+	end
+end
+
+function MunSpeak.First()	-- What this addon will do when it initializes. 
+	MunSpeak.ReadTable()
+	MunSpeak.ReadAdmins()
+end
+
+function MunSpeak.FindPlayer(target) -- Very useful function for finding a player with just a small string.  eg:  mun would find Muneris (the playerdata) else throw a chat error with more information.
+	local Target = 0
+	local TargetPlayer
+	local MunSpeakPlayerName = target
+	
+	for k,v in pairs(player.GetAll()) do
+		if (string.find(string.lower(v:Name()), string.lower(MunSpeakPlayerName)))then 
+			Target = Target + 1
+			TargetPlayer = v
+		end
+	end
+		
+	if(Target == 0) then PrintMessage(HUD_PRINTTALK,"[MunSpeak] - No player found with that name") end
+	if(Target > 1) then PrintMessage(HUD_PRINTTALK,"[MunSpeak] - Too many players found. Try refining the search criteria") end
+		
+	if(Target == 1) then
+		return TargetPlayer
+	end
+	
+end
+
+function MunSpeak.MovePlayer(ply,target,channel) -- ply is the player trying to initiate the move function, target is the player to move, and channel is the channel to move the target into.
+	if(ply:IsAdmin() or MunSpeak.IsAdmin(ply)) then
+		if(MunSpeakChannels[channel]==nil) then ply:PrintMessage(HUD_PRINTTALK,"Channel does not exist")
+			PrintMessage(HUD_PRINTTALK,channel)
+		else
+			MunSpeak.JoinChannel(target,channel,_,true) 
+		end
+	else
+		ply:PrintMessage(HUD_PRINTTALK,"You do not have the sufficient permissions")
+	end
+end
+
+function MunSpeak.CreateAdmin(ply,target)
+	if(ply:IsAdmin() and table.HasValue(MunSpeakAdmins,ply:SteamID()) == false ) then
+		target.MunSpeakAdmin = true
+		table.insert(MunSpeakAdmins,target:SteamID())
+		PrintMessage(HUD_PRINTTALK,"Made "..target:Nick().." into a MunSpeak Admin!")
+		MunSpeak.WriteAdmins()
+	else
+		if(table.HasValue(MunSpeakAdmins,ply:SteamID()) == true) then 
+			PrintMessage(HUD_PRINTTALK,"Player is already an admin") return
+		end
+			PrintMessage(HUD_PRINTTALK,"You do not have permission to do that action.")
+	end
+end
+
+net.Receive("MunSpeakClientJoin",function() -- Handles the client trying to join a channel via the UI
+local ClientTable = net.ReadTable()
+local Player = ClientTable[1]
+local Channel = ClientTable[2]
+local Password = ClientTable[3]
+
+MunSpeak.JoinChannel(Player,Channel,Password)
+end)
+
+hook.Add("PlayerSay", "munspeakcreate", MunSpeak.ChatMessages) -- When / commands are ran, this hook handles it.
+hook.Add("PlayerSpawn","MunTalkPlayerHasConnected",MunSpeak.PlayerHasConnected) -- When a player connects to the server
+hook.Add( "Initialize", "MunSpeakInit", MunSpeak.First ) -- When the addon first starts up
